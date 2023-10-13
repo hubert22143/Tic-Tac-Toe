469c3d3ccd3be9ae427acad0ec6e3f86b799fd42
hubert22143 <hubert.baciak@o2.pl>
2023-10-14 00:17:00 +0200

    Added modal congratulations block, fixed small issues

--- script.js
+++ script.js
@@ -16,56 +16,62 @@ playerButton.addEventListener('click', () => {
     beforeYouStartBox.style.flexDirection = "column";
 });
 const startGame = document.querySelector('.startGame');
+let nickname ='';
 const nicknameInput = document.getElementById('nickname');
-let nickname;
 nicknameInput.addEventListener('change', () => {
     nickname = nicknameInput.value;
     if (nickname.trim() === '') {
         alert('Nickname cannot be empty');
     }
 });
+let svgCircle = document.getElementById('svg-circle');
+let svgCross = document.getElementById('svg-cross');
 function getMarker (){
     const selectElements = document.getElementById('game-mode');
     function getSelectedValue(){
         let selectedOption = selectElements.value;
         switch(selectedOption){
             case 'option1':
-                selectedOption = 'Circle';
+                selectedOption = svgCircle;
                 break;
             case 'option2':
-                selectedOption = 'Cross';
+                selectedOption = svgCross;
                 break;
             default:
-                selectedOption = 'Circle';
+                selectedOption = svgCircle;
         }
         return {selectedOption}
     }
     return{getSelectedValue};
 }
-let chosenMarker = 'Circle';
+let chosenMarker = svgCircle;
 const checkedOption = document.getElementById('game-mode');
 checkedOption.addEventListener('change', () => {
     chosenMarker = getMarker().getSelectedValue().selectedOption;
     console.log(chosenMarker);
 });
-
-function switchPlayer(currentPlayer) {
-    if (currentPlayer === firstPlayerMarker) {
-        return secondPlayerMarker;
-    } else {
-        return firstPlayerMarker;
-    }
-}
+let currentPlayerText = document.getElementById('currentPlayer');
+currentPlayerText.textContent = `Current turn is for: ${nickname}`;
 let currentPlayer;
 let firstPlayerMarker;
 let secondPlayerMarker;
 let secondPlayerNickname;
+function switchPlayer(currentPlayer) {
+    if (currentPlayer === firstPlayerMarker) {
+        currentPlayerText.textContent = `Current turn is for player: ${secondPlayerNickname}`;
+        return secondPlayerMarker;
+    } else {
+        currentPlayerText.textContent = `Current turn is for player: ${nickname}`;
+        return firstPlayerMarker;
+    }
+}
 startGame.addEventListener('click', (e) => {
-    const nicknameValue = nicknameInput.value;
-    if (nicknameValue.trim() === '') {
+    e.preventDefault();
+    nickname = nicknameInput.value;
+    if (nickname.trim() === '') {
         e.preventDefault();
         alert('Please enter a nickname before starting the game');
-    } else if (!nicknameValue.match(/^[a-zA-Z]{3,}$/)) {
+    } else if (!nickname.match(/^[a-zA-Z]{3,}$/)) {
         e.preventDefault();
         alert('Nickname must contain at least 3 letters');
     } else {
@@ -76,26 +82,39 @@ startGame.addEventListener('click', (e) => {
     firstPlayerMarker = chosenMarker;
     console.log("The first player marker is: " ,firstPlayerMarker);
     secondPlayerNickname = "Bob";
-    if(firstPlayerMarker === 'Circle'){
-        secondPlayerMarker = 'Cross';
+    if(firstPlayerMarker === svgCircle){
+        secondPlayerMarker = svgCross;
         console.log(secondPlayerMarker);
         console.log(secondPlayerNickname);
     }else{
-        secondPlayerMarker = 'Circle';
+        secondPlayerMarker = svgCircle;
         console.log(secondPlayerMarker);
         console.log(secondPlayerNickname);
     }  
     currentPlayer = firstPlayerMarker;
+    headerText.textContent = "I hope you will enjoy your game!";
     createGameBoard();
 });
+const modalContainer = document.querySelector('.modal-container');
+modalContainer.style.display = 'none';
+let modalCongratulationsHeader = document.querySelector('.modal-header');
 const createGameBoard = () => {
     const boardContainer = document.querySelector('.playerBoard');
     const cells = Array.from({length: 9}, (_,index) => {
         const cell = document.createElement('div');
         cell.textContent = index + 1;
         cell.classList.add('boardBox');
+        function resetGame() {
+            cells.forEach((item,index) => {
+                item.textContent = index + 1;
+                item.classList.remove('filled');
+            });
+            modalContainer.style.display = 'none';
+        }
+        document.getElementById('menuResetBoard').addEventListener('click',resetGame);
+        document.getElementById('playAgain').addEventListener('click',resetGame);
         function checkForTie() {
-            return cells.every(cell => cell.textContent === 'Cross' || cell.textContent === 'Circle');
+            return cells.every(cell => cell.textContent === svgCross.textContent || cell.textContent  === svgCircle.textContent);
         }
         function checkForWin(){
             const winCombinations =[
@@ -106,8 +125,7 @@ const createGameBoard = () => {
             for(const combination of winCombinations){
                 const [a,b,c] = combination;
                 if(cells[a].textContent === cells[b].textContent &&
-                   cells[b].textContent === cells[c].textContent &&
-                   cells[c].textContent === cells[a].textContent){
+                   cells[b].textContent === cells[c].textContent){
                     return cells[a].textContent;
                    }
             }
@@ -117,20 +135,29 @@ const createGameBoard = () => {
             if(!cell.classList.contains('filled')){
                 console.log("The current player is: " , currentPlayer);
                 if(currentPlayer === firstPlayerMarker){
-                    cell.textContent = firstPlayerMarker;
+                    cell.innerHTML = '';
+                    cell.appendChild(firstPlayerMarker.cloneNode(true));
                 }else{
-                    cell.textContent = secondPlayerMarker;
+                    cell.innerHTML = '';
+                    cell.appendChild(secondPlayerMarker.cloneNode(true));
                 }
                 cell.classList.add('filled');
                 if(checkForTie()){
-                    alert(`${nickname,secondPlayerNickname} Yo! It is a tie!`)
+                    currentPlayerText.textContent = "It is a tie!";
+                    modalContainer.style.display = 'flex';
+                    modalCongratulationsHeader.textContent = "It is a tie! Rough battle!";
                     
                 }
                 const winner = checkForWin();
                 if (winner) {
                     const winningPlayer = (winner === firstPlayerMarker) ? nickname : secondPlayerNickname;
                     alert(`Player ${winningPlayer} wins!`);
-                    
+                    currentPlayerText.textContent = `Congratulations, ${winningPlayer} you have won!`;
+                    modalContainer.style.display = 'flex';
+                    modalCongratulationsHeader.textContent = `Congratulations, ${winningPlayer} you have won this rough battle!`;
+                    cells.forEach((cell) => {
+                        cell.style.pointerEvents = 'none';
+                    });
                 } else {
                     currentPlayer = switchPlayer(currentPlayer);
                 }
@@ -141,3 +168,7 @@ const createGameBoard = () => {
     cells.forEach(cell => boardContainer.appendChild(cell));
 }
 
+function displayNone(e){
+    e.target.parentNode.parentNode.parentNode.style.display = 'none';
+}
+document.getElementById('exit').addEventListener('click',displayNone);
