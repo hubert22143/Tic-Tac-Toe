547b939a42e3fd4bf281d558f928720f518bf6ce
hubert22143 <hubert.baciak@o2.pl>
2023-10-16 19:28:01 +0200

    Working upon PlayerVsAi mode

--- PlayerVsAi.js
+++ PlayerVsAi.js
@@ -1,39 +1,65 @@
-const aiBeforeContainer = document.querySelector('.AiBeforeYouStartGame');
-aiButton.addEventListener('click', () => {
-    aiBeforeContainer.style.display = 'flex';
-    playerButton.style.display = 'none';
-    aiButton.style.display = 'none';
-    headerText.textContent = 'Before you start enjoying your game...';
-})
-const aiUserNickname = document.getElementById('aiUserNickname');
-let userNickname;
-aiUserNickname.addEventListener('change', () => {
-    userNickname = aiUserNickname.value;
-});
-userNickname = aiUserNickname.value
 const aiSubmitButton = document.getElementById('aiBeforeSubmitButton');
-aiSubmitButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    entryMenu.style.display = 'none';
-    console.log(userNickname);
-})
-function getPlayerMarker(){
-    const circleInput = document.querySelector('input[name="marker"][value="aiCircle"]');
-    const crossInput = document.querySelector('input[name="marker"][value="aiCross"]');
-    if(circleInput.selected){
-        console.log("Circle is selected");
-    }else if(crossInput.selected){
-        console.log("Cross is selected");
+console.log(aiSubmitButton);
+function createGameConfiguration() {
+    const state = {
+        userNickname: '',
+        playerMarker: '',
+        aiMarker: '',
+    };
+
+    const aiButton = document.getElementById('aiButton');
+    const aiBeforeContainer = document.querySelector('.AiBeforeYouStartGame');
+    const playerButton = document.getElementById('playerButton');
+    const aiUserNickname = document.getElementById('aiUserNickname');
+
+    aiButton.addEventListener('click', () => {
+        aiBeforeContainer.style.display = 'flex';
+        playerButton.style.display = 'none';
+        aiButton.style.display = 'none';
+        headerText.textContent = 'Before you start enjoying your game...';
+    });
+
+    aiUserNickname.addEventListener('change', () => {
+        state.userNickname = aiUserNickname.value;
+    });
+    function getPlayerMarker() {
+        const circleInput = document.querySelector('input[name="marker"][value="aiCircle"]');
+        const crossInput = document.querySelector('input[name="marker"][value="aiCross"]');
+        if (circleInput.checked) {
+            state.playerMarker = circleInput.value;
+        } else if (crossInput.checked) {
+            state.playerMarker = crossInput.value;
+        } else {
+            state.playerMarker = circleInput.value
+        }
     }
-    return 0;
-}
-const loadPlayersData = (userNickname) =>{
-    const Players = {
-        playerOne: userNickname,
-        playerTwo: "Ai",
-    };
-    const Markers = {
-    };
 
+    function getAiMarker() {
+        state.aiMarker = state.playerMarker === 'aiCircle' ? 'aiCross' : 'aiCircle';
+    }
 
+    return {
+        getPlayerMarker,
+        getAiMarker,
+        loadPlayersData: () => ({
+            Players: {
+                playerOne: state.userNickname,
+                playerTwo: "Ai",
+            },
+            Markers: {
+                playerMarker: state.playerMarker,
+                aiMarker: state.aiMarker,
+            }
+        }),
+    };
 }
+const gameConfig = createGameConfiguration();
+aiSubmitButton.addEventListener('click', (e) => {
+e.preventDefault();
+console.log("yes")
+gameConfig.getPlayerMarker();
+gameConfig.getAiMarker();
+const playersData = gameConfig.loadPlayersData();
+console.log(playersData);
+entryMenu.style.display = 'none';
+})
